489cd5ddd85fea45e11ba2f41c9fee86a840ab56
Jan Uchto <juchto9@gmail.com>
2018-08-09 13:06:14 +0200

    Class addUser added - variables

---
+++ RCP/RCP/addUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCP
+{
+    class addUser
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public DateTime dateOfBirth = new DateTime();
+        public string position { get; set; }
+        public string deparment { get; set; }
+
+
+    }
+}

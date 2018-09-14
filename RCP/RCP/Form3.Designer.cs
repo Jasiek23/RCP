16cc3430c5d2bba9e9950b48f0cce2dab1a3ad77
Jan Uchto <juchto9@gmail.com>
2018-09-14 14:28:53 +0200

    Code cleaning

--- RCP/RCP/Form3.Designer.cs
+++ RCP/RCP/Form3.Designer.cs
@@ -42,7 +42,6 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(27, 145);
